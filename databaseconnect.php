2db3c1170a2ba6efc525bdb8d6b03719b26ce609
SCL3 <106513368+SCL3@users.noreply.github.com>
2024-02-17 17:08:29 +0100

    Add files via upload

--- databaseconnect.php
+++ databaseconnect.php
@@ -6,7 +6,7 @@ try {
     $bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);  //Base de donnee SQL
     // Définir les options PDO pour afficher les erreurs SQL
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connecté sur : $dbname à : $host réussie !<br>";  //On montre a l'ecran que ca a bien fonctionne
+    //echo "Connecté sur : $dbname à : $host réussie !<br>";  //On montre a l'ecran que ca a bien fonctionne
 
 } 
 catch (PDOException $pe) {
