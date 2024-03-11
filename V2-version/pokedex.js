7c6223aa2ef22a5aa0f21cc7c901514e316583b7
Nagulanathan Dines <104365459+Dines2593@users.noreply.github.com>
2024-03-11 11:55:48 +0100

    Add files via upload

---
+++ V2-version/pokedex.js
@@ -0,0 +1,11 @@
+const MAX_POKEMON = 151;
+const MIN_POKEMON = 1;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector(".search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+
+
